a7db40ae11d063dfca8d6a1b9cb068084aa21dea
Pedro Poblete Lasserre <pedro@pobletelasserre.me>
2018-09-04 13:42:55 +0200

    Updated API to prevent CORS errors

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ db.initialize()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use( (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+} )
+
 const publicRoutes =  require('./api/routes/public')
 const privateRoutes = require('./api/routes/privates')
 
