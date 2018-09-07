ec443f32780df1f7ae300a10885b35e7c821d0ab
Pedro Poblete Lasserre <pedro@pobletelasserre.me>
2018-09-08 00:32:37 +0200

    Encryption, Administration, and Users: - Passwords and Emails are Encrypted - Added a Schema to the DB and Administration to it - Using environment variables instead of hard-coded variables - Finalized public routes for questions - Added users routes

---
+++ api/middleware/email.js
@@ -0,0 +1,19 @@
+const validator = require('validator');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr(process.env.CRYPTRKEY);
+
+
+exports.encryptEmail = (req, res, next) => {
+  if (!req.body.email) {
+    return res.status(400).json({
+      error: "Email not included."
+    })
+  }
+  else if (!validator.isEmail(req.body.email)) {
+    return res.status(400).json({
+      error: "Invalid email."
+    })
+  }
+  req.body.email = cryptr.encrypt(req.body.email)
+  next()
+}
--- api/models/validation.js
+++ api/models/validation.js
@@ -1,17 +1,25 @@
 const assert = require('assert')
 
-exports.validateAskedQuestion = function(question) {
-  try {
-    assert(question.id)
-    assert(question.text.en || question.text.de)
-  }
-  catch(err) {
-    return false
-  }
-}
 
-exports.validateRegisteredQuestion = function(question) {
-  try {
+// TODO: THIS CAN BE USED TO CHECK WHETHER THE REQUEST IS GOOD OR NOT. RIGHT NOW NOT BEING USED
+// exports.validateAskedQuestion = (req, res, next) => {
+//   if (!req.body.text) {
+//     res.status(400).json({
+//       error: "No question sent to the server. Please try again."
+//     })
+//   }
+//   else if (!req.body.text.en || !req.body.text.de) {
+//     res.status(400).json({
+//       error: "The question sent is not in a valid language."
+//     })
+//   }
+//   else {
+//     try {
+//       next()
+//     }
+//   }
+// }
 
-  }
-}
+
+
+// TODO: SHOULD I WRITE TESTS?
